9b78d394cc979e1c492314243b6340232386e6d3
tlburris3 <tlburris11@gmail.com>
2018-04-30 16:33:18 +0000

    Changing files and adding files in order to follow along with video from the online course.

--- IntroToExpress/EJSDemo/app.js
+++ IntroToExpress/EJSDemo/app.js
@@ -3,12 +3,24 @@ var app = express();
 
 /** ROUTES **/
 app.get("/", function(req, res) {
-   res.send("<h1>Welcome to the homepage!</h1>" +
-            "<h2>New line</h2>");
-            
+    var type = req.params.type;
     res.render("home.ejs");
 });
 
+app.get("/fallinlovewith/:dog", function(req, res) {
+   var dogType = req.params.dog;
+   res.render("love.ejs", {dogType, dogType});
+});
+
+app.get("/posts", function(req, res) {
+   var posts = [{title: "post 1", author: "Susy"},
+                {title: "Simple Addition to Life", author: "Charlie"},
+                {title: "Finding the Answer", author: "Terry"}
+                ];
+   res.render("posts.ejs", {posts, posts});
+});
+
+/** Starting the server **/
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("Server started...");
 });
