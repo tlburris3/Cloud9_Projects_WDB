636570ab90cf58ea06528b303e178fa7891eea96
tlburris3 <tlburris11@gmail.com>
2018-05-08 15:53:15 +0000

    Going based off of RESTful Routing pattern for the rest of the project. Created a new show page that is going to demonstrate the information of a campsite, looking that specific campsite based off of an id.

--- YelpCampVersions/v2/app.js
+++ YelpCampVersions/v2/app.js
@@ -10,10 +10,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Schema setup
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   image: String
+   image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//    {
+//       name: "Granite Hill",
+//       image: "https://farm5.staticflickr.com/4100/4798314980_bc31231984.jpg",
+//       description: "A place that is terrifying to look at, but brings you freedom when at the top!" 
+//    }, 
+//    function (err, cg) {
+//       if (err)
+//          console.log("ERROR: " + err);
+//       else
+//          console.log("Added a new campground: " + cg);
+//    }
+// );
 
 /** ROUTES **/
 // Root Route - Landing Page
@@ -21,24 +36,27 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX route - show all campgrounds
 app.get("/campgrounds", function(req, res) {
    // Get all campgrounds from DB, then render file.
    Campground.find({}, function(err, allCampgrounds) {
       if (err)
          console.log("ERROR: " + err);
       else
-         res.render('campgrounds', {campgrounds: allCampgrounds});
+         res.render('index', {campgrounds: allCampgrounds});
    });
    
    // Show the campgrounds
    // res.render('campgrounds', {campgrounds: campgrounds});
 });
 
+// CREATE route - add new campground to DB
 app.post("/campgrounds", function(req, res) { /** RESTful Routing - just the basics so far **/
    // get data from form
    var campName = req.body.name;
    var campImage = req.body.image;
-   var newCampGround = {name: campName, image: campImage};
+   var campDesc = req.body.description;
+   var newCampGround = {name: campName, image: campImage, description: campDesc};
    // Add a campground to the campgrounds array
    // campgrounds.push(newCampGround);
    
@@ -52,9 +70,23 @@ app.post("/campgrounds", function(req, res) { /** RESTful Routing - just the bas
    });
 });
 
+// NEW route - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
    // show form to submit the post request
    res.render("new");
+});
+
+// SHOW - shows info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+   // find the campground with provided ID
+   Campground.findById(req.params.id, function(err, foundCampground) {
+      if (err) {
+         console.log(err);
+      } else {
+         // render show template with that campground
+         res.render("show", {campground: foundCampground});
+      }
+   });
 });
 
 /** Starting the Server **/
